2c6d1fd090c8986fb085ffa438f593222b3015f4
JOEY <puiying526@gmail.com>
2023-04-15 16:16:00 +0100

    work on the dropdown menu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const menuBtn = document.getElementById('menuBtn');
 const menu = document.getElementById('menu');
-const main = document.getElementById('main');
+
 
 menuBtn.addEventListener('click',() => {
-    menu.classList.toggle('flex'); // Display flex menu
-    menu.classList.toggle('hidden'); // Hidden menu 
+   
+    menu.classList.toggle('hidden');
 })
 
 
